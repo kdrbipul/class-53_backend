cea49cf84615f2d5e2249f3e31a9702494804d1f
kdrbipul <kdrbipul@gmail.com>
2023-10-19 09:33:09 +0600

    first commit taday

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
@@ -55,9 +56,9 @@ const users = [
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}<password>@cluster0.ocxmtap.mongodb.net/?retryWrites=true&w=majority`;
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4que1ds.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -68,18 +69,27 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try{
+        const database = client.db('simpleNode');
+        const userCollection = database.collection('users')
+        // const user = {name:"Abdul Kader", age: 23,}
+        // const result = await userCollection.insertOne(user);
+        // console.log(result);
+
+        app.post('/users', async (req,res)=>{
+            // console.log(req);
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+  
 }
 run().catch(console.dir);
+
 
 
 app.get('/',(req, res) =>{
@@ -90,23 +100,18 @@ app.get('/users', (req, res) =>{
     res.send(users)
 })
 
-app.post('/users', (req,res)=>{
-    console.log(req);
-    const user = (req.body);
-    users.push(user);
-    res.send(users)
-})
 
-app.get('/users', (req, res) => {
-    // console.log(req.query.name);
-    if(req.query.name){
-        const searched = req.query.name;
-        const filtered = users.filter(user => user.name === searched);
-        res.send(filtered);
-    }else{
-        res.send(users)
-    }
-})
+
+// app.get('/users', (req, res) => {
+//     // console.log(req.query.name);
+//     if(req.query.name){
+//         const searched = req.query.name;
+//         const filtered = users.filter(user => user.name === searched);
+//         res.send(filtered);
+//     }else{
+//         res.send(users)
+//     }
+// })
 
 app.listen(port, () =>{
     console.log(`our server is successfully run ${port}`);
